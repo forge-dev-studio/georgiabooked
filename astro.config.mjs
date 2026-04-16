d7240d83723e158932a94be20248421ebc293067
Corey Redwine <75434996+credwine@users.noreply.github.com>
2026-04-16 02:15:39 -0400

    feat: add header, footer, brand assets, and vite path aliases

--- astro.config.mjs
+++ astro.config.mjs
@@ -14,5 +14,12 @@ export default defineConfig({
   },
   vite: {
     ssr: { noExternal: ['satori'] },
+    resolve: {
+      alias: {
+        '@lib': new URL('./src/lib', import.meta.url).pathname,
+        '@components': new URL('./src/components', import.meta.url).pathname,
+        '@layouts': new URL('./src/layouts', import.meta.url).pathname,
+      },
+    },
   },
 });
